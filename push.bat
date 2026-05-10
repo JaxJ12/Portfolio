925793c92b0c104b9f986120c127de664c5d6b38
Jax Johnson <jaxjohnson179263@gmail.com>
2026-05-09 21:57:26 -0500

    Restore dashboard layout HTML accidentally deleted during bot removal

--- push.bat
+++ push.bat
@@ -5,6 +5,6 @@ if exist ".git\index.lock" del /f ".git\index.lock"
 if exist ".git\index" del /f ".git\index"
 git reset HEAD
 git add -A
-git commit -m "Fix dashboard reveal hidden iframes and dark chatbot on project pages"
+git commit -m "Restore dashboard layout HTML accidentally deleted during bot removal"
 git push origin main
 pause
