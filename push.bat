49b54d7f07f5fc2d2bcdcbf5b1c748c9f9050580
Jax Johnson <jaxjohnson179263@gmail.com>
2026-05-09 20:57:14 -0500

    Move View Webpage to card top row, remove chatbot from all integrated pages

--- push.bat
+++ push.bat
@@ -5,6 +5,6 @@ if exist ".git\index.lock" del /f ".git\index.lock"
 if exist ".git\index" del /f ".git\index"
 git reset HEAD
 git add -A
-git commit -m "Update chatbot knowledge base, fix LinkedIn reposts, card UX improvements, skills nav fix"
+git commit -m "Move View Webpage to card top row, remove chatbot from all integrated pages"
 git push origin main
 pause
