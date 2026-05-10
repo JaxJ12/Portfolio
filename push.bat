2186c354da984f61fa2fb87075c21b9e81680243
Jax Johnson <jaxjohnson179263@gmail.com>
2026-05-09 20:22:56 -0500

    Fix preview JS, skills nav, remove journalism game bot, reroute portfolio.html to index

--- push.bat
+++ push.bat
@@ -2,6 +2,8 @@
 cd /d "%~dp0"
 if exist ".git\HEAD.lock" del /f ".git\HEAD.lock"
 if exist ".git\index.lock" del /f ".git\index.lock"
+if exist ".git\index" del /f ".git\index"
+git reset HEAD
 git add -A
 git commit -m "Fix preview JS, skills nav, remove journalism game bot, reroute portfolio.html to index"
 git push origin main
